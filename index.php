7fa899f69dc5605071f09897322943710f380b07
RolandoPankow <84215835+RolandoPankow@users.noreply.github.com>
2022-10-17 16:52:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistemas y Aplicaciones</title>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="num2letras.js"></script>
+</head>
+<body>
+<h3> simple Función Javascript para pasar números decimales a letras</h3>
+<h3> INF: escriba en el cuadro de texto el número decimal a convertir<br>
+y mediante el evento onkeyup se monstrará en resultado en el un div...</h3>
+<input id="textnum" type="number" onkeyup="numxletras()" value="">
+    <div>Resultado:</div><div id="result"></div>
+    <br>
+    <br>
+    by: <a href="rolandopankow@gmail.com">rolandopankow@gmail.com</a>
+    <br><a href="sisapp.pro">Sistemas y Aplacaciones Profesionales</a>
+</body>
+</html>
